fac9575fe49e9a629cd9e06d15b7c04d64b81777
Maksym Druziuk <druziukmaksym@gmail.com>
2025-12-11 22:52:07 +0100

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script>
+  import data from './datas/data.json';
+  import Header from './components/Header.vue';
+
+  export default {
+    components: { Header },
+    data() {
+      return {
+
+      }
+    },
+
+  }
 
 </script>
 
 <template>
+  <Header></Header>
   <main>
     <router-view :key="$route.fullPath"/>
   </main>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,33 @@
 <script>
-
+export default {
+  name: 'Header',
+  data() {
+    return {}
+  }
+}
 </script>
 
 <template>
-    <h1>Header</h1>
+    <header>
+        <nav>
+            <router-link to="/" class="">
+                Cookbook
+            </router-link>
+            <router-link to="/" active-class="text-green-700" class="">
+                Home
+            </router-link>                        
+            <router-link to="/recipes" active-class="text-green-700" class="">
+                Recipes
+            </router-link>                        
+            <router-link to="/favorites" active-class="text-green-700" class="">
+                Favorites
+            </router-link>
+            <router-link to="/addrecipe" class="text-green">
+                Add Recipe
+            </router-link>
+        </nav>
+    </header>
 </template>
 
 <style>
-    
 </style>
