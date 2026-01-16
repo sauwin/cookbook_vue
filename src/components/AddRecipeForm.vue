b6ca7ecf1a2aa753db74109d2b4bbbec4d5e8b56
Maksym Druziuk <druziukmaksym@gmail.com>
2026-01-16 15:13:04 +0100

    update AddRecipeForm
    upgrade the system of validation

--- src/components/AddRecipeForm.vue
+++ src/components/AddRecipeForm.vue
@@ -14,32 +14,47 @@ export default {
                 time: null,
                 image: "",
                 favorites: false
-            }
+            },
+            successMessage: "",
+            errorMessage: ""
         };
     },
     methods: {
         submitForm() {
+            const emptyFields = ["title", "slug", "time", "description", "ingredients", "steps"].filter(f => !this.recipe[f])
+
+            if (emptyFields.length) {
+                this.errorMessage = `Please fill in: ${emptyFields.join(", ")}`
+                return
+            }
+
             const store = useRecipesStore()
             store.addRecipe({
                 ...this.recipe,
                 id: Date.now(),
                 ingredients: this.recipe.ingredients.split("\n"),
-                steps: this.recipe.steps.split("\n")
+                steps: this.recipe.steps.split("\n"),
+                image: this.recipe.image + '.jpg'
             })
 
             this.recipe = {
-            title: "",
-            slug: "",
-            description: "",
-            ingredients: "",
-            steps: "",
-            time: null,
-            image: "",
-            favorites: false
+                title: "",
+                slug: "",
+                description: "",
+                ingredients: "",
+                steps: "",
+                time: null,
+                image: "",
+                favorites: false
             }
+
+            this.successMessage = 'Recipe successfully added!'
+            this.errorMessage = ""
+            setTimeout(() => this.successMessage = "", 3000)
         }
+
     }
-};
+}
 </script>
 
 <template>
@@ -47,17 +62,21 @@ export default {
     <v-text-field v-model="recipe.title" label="Title" required/>
     <v-text-field v-model="recipe.slug" label="Slug (omelette)" required/>
     <v-text-field v-model="recipe.time" type="number" label="Cooking time (minutes)"/>
-    <v-text-field v-model="recipe.image" label="Image filename (omelette.jpg)"/>
+    <v-text-field v-model="recipe.image" label="Image filename (omelette)"/>
     <v-textarea v-model="recipe.description" label="Description" rows="3"/>
     <v-textarea v-model="recipe.ingredients" label="Ingredients (one per line)" rows="4"/>
     <v-textarea v-model="recipe.steps" label="Steps (one per line)" rows="4"/>
+
+    <v-alert v-if="errorMessage" type="error" class="mt-4">
+        {{ errorMessage }}
+    </v-alert>
+
+    <v-alert v-if="successMessage" type="success" class="mt-4">
+        {{ successMessage }}
+    </v-alert>
+
     <div align="right">
         <v-btn type="submit" color="#2b9931" size="large" class="mt-4 mb-6 mr-16">Add recipe</v-btn>
     </div>
 </v-form>
 </template>
-
-
-<style>
-    
-</style>
